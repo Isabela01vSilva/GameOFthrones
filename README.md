c34caa131e485ebb68017fcca3f24ebd4f9a1d4f
Isabela01vSilva <61556971+Isabela01vSilva@users.noreply.github.com>
2020-06-22 16:38:50 -0300

    Projeto finalizado

--- README.md
+++
@@ -1,2 +0,0 @@
-# Game-of-thrones
-Sobre a serie Game of thrones-ProjetoID
--- js/app.js
+++ js/app.js
@@ -1,40 +1,56 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
 
-//buttons
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
 
-//counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target) - 75;
+  scrollToPosition(to);
+}
 
-//button listeners
-nextBtn.addEventListener('click', () => {
-  if (counter >= carouselImages.length - 1) return;
-carouselSlide.style.transition = "transform 0.4s ease-in-out";
-counter++;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
+function scrollToPosition(to) {
+  // window.scroll({
+  //   top: to,
+  //   behavior: "smooth",
+  // });
+  smoothScrollTo(0, to);
+}
 
-prevBtn.addEventListener('click', () => {
-    if (counter <= 0 ) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter --;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    });
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int} endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
 
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'lastClone'){
-        carouselSlide.style.transition = 'none';
-        counter = carouselImages.length - 2;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  duration = typeof duration !== 'undefined' ? duration : 400;
+
+  // Easing function
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
     }
-    if (carouselImages[counter].id === 'firstClone'){
-        carouselSlide.style.transition = 'none';
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-});
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
